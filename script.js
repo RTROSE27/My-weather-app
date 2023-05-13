3ca50eba9fd4b7f5ee9db1d274fdc56ae3b7cfcc
RTROSE27 <128533621+RTROSE27@users.noreply.github.com>
2023-05-13 13:46:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,9 @@ function formatDate(date) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  return day;
+  return days[day];
 }
 
 function displayForecast(response) {
