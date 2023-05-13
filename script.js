0987ec937f103b2ef71ea356a3027d862cabfd96
RTROSE27 <128533621+RTROSE27@users.noreply.github.com>
2023-05-13 13:21:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function displayForecast(response) {
             <div class="col-2">
               <div class="weather-day">${forecastDay.dt}</div>
               <img
-                src="https://openweathermap.org/img/wn/04d@2x.png"
+                src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                 alt=""
                 width="36"
               />
