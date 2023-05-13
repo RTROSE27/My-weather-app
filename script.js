ac7903cd3433a3b3c0624aac46c3c936459b49d4
RTROSE27 <128533621+RTROSE27@users.noreply.github.com>
2023-05-13 12:31:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,9 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
 }
 
 function displayWeather(response) {
